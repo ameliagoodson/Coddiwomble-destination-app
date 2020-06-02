88cc5c478556efd1d900deebef28c0d4c0878712
Rhys Rushton <rhys.rushton@gmail.com>
2020-06-02 10:19:23 +1000

    returning user info to front end

--- views/js/landing.js
+++ views/js/landing.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
   var blogContainer = $(".blog-container"); 
+  
+  $.get("/api/landing").then(function(data){
+    console.log({data});
+    let userInfo = data[0].location;
+    console.log(userInfo); 
+
+  })
 
 
 
