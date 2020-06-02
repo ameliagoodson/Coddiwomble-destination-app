dd861c023382d2232fce8d782f842bf1ea8c8436
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-06-02 19:24:19 +1000

    add heroku

--- models/index.js
+++ models/index.js
@@ -5,7 +5,13 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
-var config    = require(__dirname + "/../config/config.json")[env];
+var config;
+if (process.env.JAWSDB_URL) {
+  config = process.env.JAWSDB_URL; 
+}
+else {
+  config    = require(__dirname + "/../config/config.json")[env];
+}
 var db        = {};
 
 if (config.use_env_variable) {
