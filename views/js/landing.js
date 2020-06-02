cc5b0a50f0f45af2ec84cdae011fa64eba9b89ca
Rhys Rushton <rhys.rushton@gmail.com>
2020-06-02 10:34:45 +1000

    location info returning. Need to add user info now.

--- views/js/landing.js
+++ views/js/landing.js
@@ -3,9 +3,17 @@ $(document).ready(function(){
   var blogContainer = $(".blog-container"); 
   
   $.get("/api/landing").then(function(data){
-    console.log({data});
-    let userInfo = data[0].location;
-    console.log(userInfo); 
+    //console.log({data});
+    let destinations = data;
+    console.log(destinations); 
+
+    for(let index= 0; index < destinations.length; index++){
+      const destination = destinations[index];
+      let list = $("<li>"); 
+      list.text(destination.location);
+      $(blogContainer).append(list);
+
+    }
 
   })
 
